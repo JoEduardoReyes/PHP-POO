8f4ea1a775d8157ca14d83bd291f2a1bd47eb8c8
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-03-14 10:33:08 -0600

    Herencia

---
+++ herencia/index.php
@@ -0,0 +1,18 @@
+<?php
+
+class User {
+  public $name;
+  public function __construct($name) {
+    $this->name = $name;
+  }
+  final public function getName() {
+    return $this->name;
+  }
+}
+
+class Admin extends User {
+  // ...
+}
+
+$admin = new Admin('Eduardo');
+echo $admin->getName(); // Eduardo
