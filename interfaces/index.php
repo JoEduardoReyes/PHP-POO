9fb7f9aa8203577a3ffa998d5487fbaed274ead3
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-03-14 10:46:15 -0600

    interfaces

---
+++ interfaces/index.php
@@ -0,0 +1,20 @@
+<?php
+// contrato
+interface Person {
+  public function getName();
+}
+
+class Admin implements Person {
+  public $name;
+
+  public function __construct($name) {
+    $this->name = $name;
+  }
+
+  public function getName() {
+    return $this->name;
+  }
+}
+
+$admin = new Admin('Eduardo');
+echo $admin->getName(); // Eduardo
