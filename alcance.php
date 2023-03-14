38f8f8e19b8294367638d7eb641825a158772542
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-03-14 09:44:59 -0600

    Alcance

---
+++ alcance.php
@@ -0,0 +1,15 @@
+<?php
+
+class user {
+  // public //
+  // protected //
+  // private //
+  public const PAGINATE = 25;
+
+  public $username;
+  // protected $username;
+  // private $username;
+  public function getUsername() {
+    
+  }
+}
